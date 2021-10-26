2422f4763e16f957286fc399789ac1d6ff298ebe
Simon Dean <simon@simondean.org>
2021-10-26 16:55:42 +0100

    Replicate issue with Swagger lib

--- src/test/java/IssueDemoTest.java
+++ src/test/java/IssueDemoTest.java
@@ -1,15 +1,42 @@
 import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IssueDemoTest {
 
     @Test
-    public void willFail() throws NoSuchFieldException, IllegalAccessException {
+    public void swaggerWillFail() {
+        OpenAPI api = loadSpecViaSwaggerLib();
+        assertRequestBodyDescription(api);
+    }
+
+    @Test
+    public void swaggerRequestValidatorLibWillFail() throws NoSuchFieldException, IllegalAccessException {
+        OpenAPI api = loadSpecViaSwaggerRequestValidatorLib();
+        assertRequestBodyDescription(api);
+    }
+
+    private OpenAPI loadSpecViaSwaggerLib() {
+        final ParseOptions parseOptions = new ParseOptions();
+        parseOptions.setResolve(true);
+        parseOptions.setResolveFully(true);
+        parseOptions.setResolveCombinators(false);
+        OpenAPIParser openAPIParser = new OpenAPIParser();
+        SwaggerParseResult swaggerParseResult = openAPIParser.readLocation("example-openapi.yaml", List.of(), parseOptions);
+        assertEquals(0, swaggerParseResult.getMessages().size());
+        OpenAPI api = swaggerParseResult.getOpenAPI();
+        return api;
+    }
+
+    private OpenAPI loadSpecViaSwaggerRequestValidatorLib() throws NoSuchFieldException, IllegalAccessException {
         OpenApiInteractionValidator.Builder builder =
                 OpenApiInteractionValidator.createFor("example-openapi.yaml");
         builder.build();
@@ -17,8 +44,11 @@ public class IssueDemoTest {
         Field apiField = builder.getClass().getDeclaredField("api");
         apiField.setAccessible(true);
         OpenAPI api = (OpenAPI) apiField.get(builder);
+        return api;
+    }
 
-        assertEquals("POST example", api.getPaths()
+    private void assertRequestBodyDescription(OpenAPI api) {
+        assertEquals("POST Example", api.getPaths()
                 .get("/example")
                 .getPost()
                 .getRequestBody()
